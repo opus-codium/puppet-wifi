407127ed522d31873b428baaf3e1f292a3e5b6dc
Romain Tartière <romain@blogreen.org>
2019-05-23 06:19:07 -1000

    Modernize code base

--- manifests/infrastructure.pp
+++ manifests/infrastructure.pp
@@ -7,37 +7,59 @@
 # @param ensure
 # @param ssid SSID of the network
 define wifi::infrastructure (
-  Optional[String] $psk = undef,
-  Optional[String] $uuid = undef,
-  Optional[String] $mac = undef,
-  Array[String] $dns = [],
-  String $ensure = 'present',
-  Optional[String] $ssid = $name,
+  Enum['present', 'absent'] $ensure = 'present',
+  Array[String]             $dns    = [],
+  Optional[String]          $mac    = undef,
+  Optional[String]          $psk    = undef,
+  Optional[String]          $ssid   = $name,
+  Optional[String]          $uuid   = undef,
 ) {
   include wifi
 
   if $psk {
     $real_psk = wifi::wpa_passphrase($ssid, $psk)
   }
-  if $uuid {
-    $real_uuid = $uuid
-  } else {
-    $real_uuid = sha1($title)
-  }
-  if $facts.get('os.family') == 'FreeBSD' and $ensure == 'present' {
-    concat::fragment { "/etc/wpa_supplicant.conf-${name}":
-      target  => '/etc/wpa_supplicant.conf',
-      content => template('wifi/wpa_supplicant.conf'),
-      order   => '10',
+
+  case $facts.get('os.family') {
+    'debian': {
+      if $uuid {
+        $real_uuid = $uuid
+      } else {
+        $real_uuid = sha1($title)
+      }
+
+      file { "/etc/NetworkManager/system-connections/${ssid}":
+        ensure  => $ensure,
+        owner   => 0,
+        group   => 0,
+        mode    => '0600',
+        content => epp('wifi/NetworkManagerConnection.epp',
+          {
+            dns       => $dns,
+            mac       => $mac,
+            real_psk  => $real_psk,
+            real_uuid => $real_uuid,
+            ssid      => $ssid,
+          }
+        ),
+      }
     }
-  }
-  if ($facts.get('os.family') == 'Debian' and $::networkmanagerversion) {
-    file { "/etc/NetworkManager/system-connections/${ssid}":
-      ensure  => $ensure,
-      owner   => 0,
-      group   => 0,
-      mode    => '0600',
-      content => template('wifi/NetworkManagerConnection.erb'),
+    'freebsd': {
+      if  $ensure == 'present' {
+        concat::fragment { "/etc/wpa_supplicant.conf-${name}":
+          target  => '/etc/wpa_supplicant.conf',
+          order   => '10',
+          content => epp('wifi/wpa_supplicant.conf.epp',
+            {
+              ssid     => $ssid,
+              real_psk => $real_psk,
+            }
+          ),
+        }
+      }
+    }
+    default: {
+      fail('operating system not supported')
     }
   }
 }
